89d2b60d86d3e32627c6e0d086702704e2f93a3b
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-03-20 11:25:37 -0300

    refatoração + salvar jornais seguidos (antes só lia)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import java.util.Scanner;
+import java.util.Formatter;
 import java.util.ArrayList;
 
 public class Application {
@@ -23,11 +24,11 @@ public class Application {
     // se jornalSeguido[i] então mostramos as notícias de jornais[i]
     public static boolean[] jornalSeguido = new boolean[jornais.length];
 
+    public static String diretorio;
+    public static File   arquivoJornaisSeguidos;
 
-    public static boolean carregarJornaisSeguidos() {
-        //
-        // Busca arquivo no diretório
-        //
+
+    public static boolean initArquivos() {
         String homepath;
         try {
             homepath = System.getenv("HOMEPATH");  // Geralmente C:/Users/user
@@ -36,61 +37,77 @@ public class Application {
             System.out.println("ERRO: Configure a variável HOMEPATH do seu sistema.");
             return false;
         }
-        String path = homepath + "/agregador-noticias/jornais-seguidos.txt";
-        File arquivo = new File(path);
-
-        //
-        // Lê o arquivo e configura jornalSeguido[]
-        //
-        try {
-            Scanner scn = new Scanner(arquivo);
-            scn.useDelimiter(";");
-
-            // Formato do arquivo jornais-seguidos.txt:
-            // Por exemplo, "true;false;true"
-            // carrega jornalSeguido[0] = true, jornalSeguido[1] = false, jornalSeguido[2] = true
-
-            int i;
-            for (i=0; scn.hasNext() && i < jornais.length; i++)
-                jornalSeguido[i] = Boolean.parseBoolean( scn.next() );
-
-            if (i < jornais.length) {
-                System.out.println("AVISO: jornais-seguidos.txt incompleto, faltam jornais.");
-                // Mas não precisa retornar false; só seguir o resto dos jornais por padrão
-                for (; i < jornais.length; i++)
-                    jornalSeguido[i] = true;
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Baixe o arquivo jornais-seguidos.txt de https://github.com/MateuxLucax/agregador-noticias");
-            System.out.println("e coloque-o em " + arquivo.getAbsolutePath() + ".");
-            return false;
-        }
-
+        diretorio = homepath + "/agregador-noticias";
+        arquivoJornaisSeguidos = new File(diretorio + "/jornais-seguidos.txt");
         return true;
+    }
+
+    public static void seguirJornal(int i) {
+        if (i >= 0 && i < jornalSeguido.length)
+            jornalSeguido[i] = true;
+    }
+
+    public static void naoSeguirJornal(int i) {
+        if (i >= 0 && i < jornalSeguido.length)
+            jornalSeguido[i] = false;
+    }
+
+    public static void carregarJornaisSeguidos() throws FileNotFoundException {
+        Scanner scn = new Scanner(arquivoJornaisSeguidos);
+        scn.useDelimiter(";");
+
+        // Formato do arquivo jornais-seguidos.txt:
+        // Por exemplo, "true;false;true"
+        // carrega jornalSeguido[0] = true, jornalSeguido[1] = false, jornalSeguido[2] = true
+
+        int i;
+        for (i=0; scn.hasNext() && i < jornais.length; i++)
+            jornalSeguido[i] = Boolean.parseBoolean( scn.next() );
+
+        if (i < jornais.length) {
+            System.out.println("AVISO: jornais-seguidos.txt incompleto, faltam jornais.");
+            // Mas não precisa retornar false; só seguir o resto dos jornais por padrão
+            for (; i < jornais.length; i++)
+                jornalSeguido[i] = true;
+        }
+    }
+
+    public static void salvarJornaisSeguidos() throws FileNotFoundException {
+        Formatter fmt = new Formatter(arquivoJornaisSeguidos);
+        int n = jornalSeguido.length;
+        for (int i=0; i < n-1; i++)
+            fmt.format("%b;", jornalSeguido[i]);
+        fmt.format("%b", jornalSeguido[n-1]);
+        fmt.close();
     }
 
     public static void main(String[] args) {
 
-        boolean jornaisSeguidosCarregados = carregarJornaisSeguidos();
-        if (!jornaisSeguidosCarregados) {
-            System.out.println("Não foi possível carregar os jornais seguidos.");
-            return;
-        }
+        initArquivos();
 
-        for (int i=0; i<jornais.length; i++) {
-            if (jornalSeguido[i]) {
-                Parser p = jornais[i].getParser();
+        try {
+            carregarJornaisSeguidos();
 
-                Instant now = Instant.now();
-                Instant yesterday = now.minus(1, ChronoUnit.DAYS);
-                Instant today = now.truncatedTo(ChronoUnit.DAYS);
+            for (int i = 0; i < jornais.length; i++) {
+                if (jornalSeguido[i]) {
+                    Parser p = jornais[i].getParser();
 
-                System.out.println(jornais[i].getNome());
-                ArrayList<Noticia> ns = p.getNoticias(Date.from(yesterday), Date.from(today));
-                for (Noticia n : ns)
-                    System.out.println(n);
+                    Instant now = Instant.now();
+                    Instant yesterday = now.minus(1, ChronoUnit.DAYS);
+                    Instant today = now.truncatedTo(ChronoUnit.DAYS);
+
+                    System.out.println(jornais[i].getNome());
+                    ArrayList<Noticia> ns = p.getNoticias(Date.from(yesterday), Date.from(today));
+                    for (Noticia n : ns)
+                        System.out.println(n);
+                }
             }
+
+            salvarJornaisSeguidos();
+        } catch (FileNotFoundException e) {
+            System.out.println("Algum(ns) arquivo(s) estão faltando.");
+            System.out.println("Baixe-os em https://github.com/MateuxLucax/agregador-noticias");
+            System.out.println("e coloque-os em " + diretorio);
         }
     }
 
