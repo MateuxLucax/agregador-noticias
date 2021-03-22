fcd944678f372f864e2f9c20711c3de535aa4559
MateuxLucax <mateuxlucax@gmail.com>
2021-03-21 22:12:44 -0300

    feat: add url validator on Noticia

--- src/main/java/models/Noticia.java
+++ src/main/java/models/Noticia.java
@@ -2,6 +2,8 @@ package models;
 
 import java.util.Date;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Noticia {
   
@@ -38,10 +40,8 @@ public class Noticia {
     return true;
   }
 
-
   public boolean setUrl(String url) {
-    // TODO usar regex pra detectar se é URL válida
-    if (url.isBlank()) return false;
+    if (url.isBlank() || !isValidURL(url)) return false;
     this.url = url;
     return true;
   }
@@ -74,5 +74,21 @@ public class Noticia {
             ", url='" + url + '\'' +
             ", resumo='" + resumo + '\'' +
             '}';
+  }
+
+  public static boolean isValidURL(String url) {
+    String regex = "((http|https)://)(www.)?"
+            + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+            + "{2,256}\\.[a-z]"
+            + "{2,6}\\b([-a-zA-Z0-9@:%"
+            + "._\\+~#?&//=]*)";
+
+    Pattern pattern = Pattern.compile(regex);
+
+    if (url == null) return false;
+
+    Matcher matcher = pattern.matcher(url);
+
+    return matcher.matches();
   }
 }
